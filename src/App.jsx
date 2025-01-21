838ed84024a16f0d7b7f827ba4d10fb7808de4fc
natasciarufino <natasciarufino17@gmail.com>
2025-01-21 17:38:47 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './Welcome';  
+
+function App() {
+  return (
+    <div>
+      <Welcome name="John" age={25} />  
+    </div>
+  );
+}
+
+export default App;
